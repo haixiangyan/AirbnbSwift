67c6fab852c7a3f0431f6b37c7b1d6a8a70443dd
haixiangyan <haixiang6123@gmail.com>
2023-11-12 14:16:34 +0800

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by 海怪 on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb
+//
+//  Created by 海怪 on 2023/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
